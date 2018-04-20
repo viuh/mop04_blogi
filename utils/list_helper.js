e1cf0ebb3bf61b63636135447923d453a2f2281d
viuh <37941072+viuh@users.noreply.github.com>
2018-04-21 01:19:04 +0300

    4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -25,7 +25,56 @@ const totalLikes = (blogs) => {
   }
 }
 
+const favoriteBlog = (blogs) => {
+
+  let myMax = {}
+  let maxval = -1
+
+  blogs.forEach (
+    item => {
+      if (item.likes > maxval) {
+        myMax = item
+        maxval = item.likes
+      }
+    }
+  )
+  //console.log('Most likes at:', myMax)
+  return myMax
+
+}
+
+const favAuthor = (blogs) => {
+
+  let tally = {}
+  blogs.forEach (
+    blog => {
+      //console.log('X', blog.author)
+      tally[blog.author] = (tally[blog.author] || 0 ) + 1
+    }
+  )
+
+  let authors = Object.keys(tally)
+  let myMax = {}
+  let maxval = -1
+
+  authors.forEach (
+    item => {
+      if (tally[item]> maxval) {
+        myMax = { "author": item, "blogs": tally[item] }
+        maxval = tally[item]
+      }
+    }
+  )
+
+  //console.log('countti', myMax)
+  return myMax
+}
+
+
+
 module.exports = {
   dummy ,
-  totalLikes
+  totalLikes ,
+  favoriteBlog ,
+  favAuthor
 }
