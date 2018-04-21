630c62cac70b8392308d1fdd4fd5e8a7a38032b7
viuh <37941072+viuh@users.noreply.github.com>
2018-04-22 00:05:05 +0300

    :coffee:

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -137,3 +137,19 @@ describe('Most likes for author (of many blogs)', () => {
   })
 })
 
+
+describe.skip('list helpers', () => {
+  test('dummy is called', () => {
+    const blogs = []
+
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+  })
+
+  describe('total likes', () => {
+    test('of empty list is 0', () => {
+      const result = listHelper.totalLikes(emptyList)
+      expect(result).toBe(0)
+    })
+  })
+})
